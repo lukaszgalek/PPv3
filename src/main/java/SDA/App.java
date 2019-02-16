8d4cb8c10d873cf5f357236a06689e0a966d8304
Lukasz Galek <lukasz.galek@o2.pl>
2019-02-16 10:23:50 +0100

    zadanie1

--- src/main/java/SDA/App.java
+++ src/main/java/SDA/App.java
@@ -1,13 +1,44 @@
 package SDA;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+
+        Random generator = new Random();
+        List parzyste = new ArrayList();
+        List nieparzyste = new ArrayList();
+
+
+        for (int i = 0; i < 20; i++) {
+
+            int value = generator.nextInt(10) + 1;
+            if (value % 2 == 0) {
+                if (parzyste.size() < 10) {
+                    parzyste.add(value);
+
+                }else {--i;}
+
+            } else {
+                if (nieparzyste.size() < 10) {
+                    nieparzyste.add(value);
+
+                }else { --i;}
+
+
+                }
+            }
+
+        System.out.println(parzyste);
+        System.out.println(nieparzyste);
+
+
     }
 }
---
+++ src/main/java/SDA/Lista.java
@@ -0,0 +1,5 @@
+package SDA;
+
+public class Lista {
+
+}
